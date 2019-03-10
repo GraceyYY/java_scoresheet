f1ddf115312f5f45338028fe08a38de64a594715
Grace <lunay0325@gmail.com>
2019-03-10 21:19:58 +0800

    Edit the data type of scoresBySubjects

--- src/model/Score.java
+++ src/model/Score.java
@@ -1,17 +1,19 @@
 package model;
 
 import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class Score {
-    private HashMap<String, Integer> scoresBySubjects;
+    private TreeMap<String, Integer> scoresBySubjects;
 
     public Score() {
-        this.scoresBySubjects = new HashMap<>();
+        this.scoresBySubjects = new TreeMap<>();
         this.scoresBySubjects.put("数学", -1);
         this.scoresBySubjects.put("语文", -1);
         this.scoresBySubjects.put("英语", -1);
         this.scoresBySubjects.put("编程", -1);
+        this.scoresBySubjects.comparator();
     }
 
     public int getTotalScore() {
