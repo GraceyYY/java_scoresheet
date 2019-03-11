a4d6829bd7b4b6804c83397518de85c0c56cda60
Grace <lunay0325@gmail.com>
2019-03-11 13:59:21 +0800

    Implement a method to process the input student information from user

--- src/service/UserInput.java
+++ src/service/UserInput.java
@@ -1,5 +1,9 @@
 package service;
 
+import model.Score;
+import model.Student;
+import model.TheClass;
+
 public class UserInput {
     public static boolean checkStuInfoFormat(String inputInfo) {
         return inputInfo.matches("^[\u4e00-\u9fa5]{2,4}，[0-9]{4}，([\u4e00-\u9fa5]{2}：[0-9]{1,2}[0]?，?)+$");
@@ -7,5 +11,29 @@ public class UserInput {
 
     public static boolean checkStuIdFormat(String inputId) {
         return inputId.matches("^([0-9]{4}，?)+$");
+    }
+
+    public static void processInfoInput(TheClass klass, String input) {
+        String[] info = input.split("，");
+        String name = info[0];
+        String id = info[1];
+        if (klass.isStudentInClass(id)) {
+            for (int i = 2; i < info.length; i++) {
+                String[] scores = info[i].split("：");
+                String subject = scores[0];
+                int score = Integer.valueOf(scores[1]);
+                klass.getStudentById(id).getScore().setScoresBySubjects(subject, score);
+            }
+        } else {
+            Score studentScore = new Score();
+            for (int i = 2; i < info.length; i++) {
+                String[] scores = info[i].split("：");
+                String subject = scores[0];
+                int score = Integer.valueOf(scores[1]);
+                studentScore.setScoresBySubjects(subject, score);
+            }
+            Student student = new Student(name, id, studentScore);
+            klass.updateStudent(student);
+        }
     }
 }
