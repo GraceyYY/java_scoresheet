115bdb2e71f3f1b5dd1ee1d292f6363cc562d9f6
Grace <lunay0325@gmail.com>
2019-03-11 14:45:42 +0800

    Edit the readFromFile method to avoid throw IOException when the file is empty

--- src/service/Document.java
+++ src/service/Document.java
@@ -14,10 +14,12 @@ public class Document {
 
     public Optional<TheClass> readFromFile() {
         Optional<TheClass> currentClass = Optional.empty();
-        try (ObjectInputStream inputFile = new ObjectInputStream(new FileInputStream(this.scoreFile))) {
-            currentClass = Optional.ofNullable((TheClass) inputFile.readObject());
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (this.scoreFile.length() > 0) {
+            try (ObjectInputStream inputFile = new ObjectInputStream(new FileInputStream(this.scoreFile))) {
+                currentClass = Optional.ofNullable((TheClass) inputFile.readObject());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return currentClass;
     }
