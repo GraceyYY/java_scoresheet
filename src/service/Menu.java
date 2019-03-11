cebce4c4fa46cbc240320ecdae44f1200e28ff3a
Grace <lunay0325@gmail.com>
2019-03-11 14:48:18 +0800

    Implement the function that student information is received from local file and the update is uploaded to the file promptly

--- src/service/Menu.java
+++ src/service/Menu.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 
 public class Menu {
     public static void showMenu() {
+        Document doc = new Document();
+        TheClass klass = doc.readFromFile().orElseGet(() -> new TheClass());
         Scanner scanner = new Scanner(System.in);
         System.out.println("1. 添加学生\n" +
                 "2. 生成成绩单\n" +
@@ -22,7 +24,8 @@ public class Menu {
                     Print.showInfoFormatErrorHint();
                     input = scanner.next();
                 }
-                UserInput.processInfoInput(klass,input);
+                UserInput.processInfoInput(klass, input);
+                doc.writeToFile(klass);
                 Menu.showMenu();
                 break;
             }
@@ -33,10 +36,10 @@ public class Menu {
                     Print.showIdFormatErrorHint();
                     input = scanner.next();
                 }
-                String[] subjects = {"数学","语文","英语","编程"};
+                String[] subjects = {"数学", "语文", "英语", "编程"};
                 Scoresheet scoresheet = new Scoresheet(subjects);
-                List<String> ids = UserInput.processIdInput(klass,input);
-                Print.printScoresheet(scoresheet.generateScoresheet(ids,klass));
+                List<String> ids = UserInput.processIdInput(klass, input);
+                Print.printScoresheet(scoresheet.generateScoresheet(ids, klass));
                 Menu.showMenu();
                 break;
             }
